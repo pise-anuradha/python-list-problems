e943477837fd42692e29bd942a901c6cc0c7972c
pise-anuradha <167015021+pise-anuradha@users.noreply.github.com>
2024-04-21 16:24:35 +0530

    Update main.py

--- problem - 10/main.py
+++ problem - 10/main.py
@@ -1,1 +1,9 @@
-
+list10 = ['Sakshi','Apurvaa','Shruti','Radha','Asha','Nutan','Pranjali']
+def wordCount(listName):
+  count = []
+  length = int(input('Enter the length  : '))
+  for l in listName:
+    if len(l)>length:
+      count.append(l)
+  return count
+wordCount(list10)
